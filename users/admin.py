d6cba6e2d30bd4a95d1e8ff88814ad88c41a6398
seoah <24seoah@sookmyung.ac.kr>
2025-07-09 14:05:32 +0900

    Feat : 이름, 이메일 추가

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['user_id', 'name', 'email']
+    fieldsets = (
+        (None, {'fields': ('user_id', 'name', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'fields': ('user_id', 'name', 'email', 'password1', 'password2'),
+        }),
+    )
+    ordering = ('user_id',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
