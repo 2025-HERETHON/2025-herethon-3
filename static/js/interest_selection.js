51cce89a461048d902a4ae4bda65090ee554cf5c
kimyeoneun <kye00150@naver.com>
2025-07-11 01:55:47 +0900

    연동 버전 / 관심사 선택, 직무 추천

--- static/js/interest_selection.js
+++ static/js/interest_selection.js
@@ -1,5 +1,3 @@
-const tags = document.querySelectorAll('.tag');
-
 const submitBtn = document.getElementById("submit_btn");
 const tagButtons = document.querySelectorAll(".tag");
 
@@ -19,4 +17,21 @@ tagButtons.forEach((btn) => {
       submitBtn.style.color = "#fff";
     }
   });
+});
+
+// form 제출 전에 선택된 태그들 input에 넣기
+const form = document.querySelector("form");
+const interestsInput = document.getElementById("interestsInput");
+
+form.addEventListener("submit", (e) => {
+  const selectedTags = Array.from(document.querySelectorAll(".tag.selected"))
+    .map(tag => tag.textContent.trim());
+
+  if (selectedTags.length === 0) {
+    e.preventDefault(); // 선택 안 했으면 제출 막기
+    alert("관심사를 하나 이상 선택해주세요!");
+    return;
+  }
+
+  interestsInput.value = selectedTags.join(",");
 });
